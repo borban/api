f2d3c68512d12ede9561c71e1f9dade9a2872451
Benjamin Orban <ben@orbanfamily.org>
2023-07-06 12:59:01 -0500

    Initial commit

---
+++ src/test/java/com/orbansolutions/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbansolutions.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
