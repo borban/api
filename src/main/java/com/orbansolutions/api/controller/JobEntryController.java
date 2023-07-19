1ec9825843da9cfcf8f29524d901c7fb87ea7f75
Benjamin Orban <ben@orbanfamily.org>
2023-07-19 17:01:14 -0500

    Added sorting to job entry retrieval

--- src/main/java/com/orbansolutions/api/controller/JobEntryController.java
+++ src/main/java/com/orbansolutions/api/controller/JobEntryController.java
@@ -3,6 +3,7 @@ package com.orbansolutions.api.controller;
 import com.orbansolutions.api.model.JobEntry;
 import com.orbansolutions.api.repository.JobEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,6 @@ public class JobEntryController {
 
     @GetMapping
     public List<JobEntry> getAllJobEntries(){
-        return jobEntryRepository.findAll();
+        return jobEntryRepository.findAll(Sort.by(Sort.Direction.DESC, "targetDate"));
     }
 }
